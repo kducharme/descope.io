e7fba9cd41c35b3c4790e48511d3a53e3536253f
Kyle Ducharme <ducharme.kyle@gmail.com>
2021-12-24 15:53:53 -0500

    initial commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,52 @@
+<template>
+  <header>
+    <nav class="nav">
+      <div class="nav__left">
+        <p>Logo here</p>
+      </div>
+      <div class="nav__right">
+        <router-link class="nav__link" :to="{ name: 'Home' }">Home</router-link>
+        <router-link class="nav__link" :to="{ name: 'Register' }">Create</router-link>
+        <router-link class="nav__link" :to="{ name: 'Login' }">Login</router-link>
+        <section>Logout</section>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  setup() {
+    // Get user from store
+
+    // Setup ref to router
+
+    // Logout function
+
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  display: flex;
+  justify-content: space-between;
+  height: 72px;
+  padding: 0 24px;
+  box-shadow: rgb(0 0 0 / 8%) 0px 1px 12px !important;
+  background: white;
+  .nav__left,
+  .nav__right {
+    display: flex;
+    align-items: center;
+    width: 50%;
+  }
+  .nav__right {
+    justify-content: flex-end;
+  }
+  .nav__link {
+    padding: 0 12px;
+  }
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <!-- Error Handling -->
+    <div v-if="errorMsg">
+      <p>{{ errorMsg }}</p>
+    </div>
+
+    <!-- Login Form -->
+    <form>
+      <h1>Log in</h1>
+      <div>
+        <label for="email">Email</label>
+        <input type="text" required id="email" v-model="email" />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="password" required id="password" v-model="password" />
+      </div>
+      <button type="submit">Log in</button>
+      <router-link :to="{ name: 'Register' }">Don't have an account? Sign up.</router-link>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "login",
+  setup() {
+    // Create data / vars
+    const email = ref(null);
+    const password = ref(null);
+    const errorMsg = ref(null);
+
+    // Register function
+
+    return { email, password, errorMsg };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+form {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin: 16px;
+}
+
+form > div {
+  display: flex;
+  flex-direction: column;
+  margin: 8px 0;
+}
+
+input {
+  border: 1px solid gray;
+}
+
+button {
+  background: black;
+  color: white;
+}
+
+</style>
