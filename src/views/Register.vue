14dd69a537fd0f1b978568c2ded040d1852d5a02
Kyle Ducharme <ducharme.kyle@gmail.com>
2021-12-24 19:56:50 -0500

    auth working

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,7 +6,7 @@
     </div>
 
     <!-- Registration Form -->
-    <form>
+    <form @submit.prevent="register">
       <h1>Create new account</h1>
       <div>
         <label for="email">Email</label>
@@ -18,35 +18,66 @@
       </div>
       <div>
         <label for="confirmPassword">Confirm Password</label>
-        <input type="password" required id="confirmPassword" v-model="confirmPassword" />
+        <input
+          type="password"
+          required
+          id="confirmPassword"
+          v-model="confirmPassword"
+        />
       </div>
       <button type="submit">Sign up</button>
-      <router-link :to="{ name: 'Login' }">Already have an account? Log in.</router-link>
+      <router-link :to="{ name: 'Login' }"
+        >Already have an account? Log in.</router-link
+      >
     </form>
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
+import { supabase } from "../supabase/init";
+import { useRouter } from "vue-router";
 
 export default {
   name: "register",
   setup() {
     // Create data / vars
+    const router = useRouter();
     const email = ref(null);
     const password = ref(null);
     const confirmPassword = ref(null);
     const errorMsg = ref(null);
 
     // Register function
+    const register = async () => {
+      if (password.value === confirmPassword.value) {
+        try {
+          let { error } = await supabase.auth.signUp({
+            email: email.value,
+            password: password.value,
+          });
+          if (error) throw error;
+          router.push({ name: "Login" });
+        } catch (error) {
+          errorMsg.value = error.message;
+          setTimeout(() => {
+            errorMsg.value = null;
+          }, 5000);
+        }
+        return;
+      }
+      errorMsg.value = "Error: Passwords do not match. Please try again.";
+      setTimeout(() => {
+        errorMsg.value = null;
+      }, 5000);
+    };
 
-    return { email, password, confirmPassword, errorMsg };
+    return { email, password, confirmPassword, errorMsg, register };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-
 form {
   display: flex;
   flex-direction: column;
@@ -68,5 +99,4 @@ button {
   background: black;
   color: white;
 }
-
 </style>
