51f543768d72704e6f7b73fc46fa08370cf27554
Kyle Ducharme <ducharme.kyle@gmail.com>
2022-02-28 13:46:31 -0500

    drag and drop working

--- src/components/single/TheCreateFeedbackModal.vue
+++ src/components/single/TheCreateFeedbackModal.vue
@@ -156,49 +156,78 @@ export default {
       // Make the DIV element draggable:
       const element = document.querySelector("#createFeedback");
 
-      console.log(element);
+      var PADDING = 8;
+
+      var rect;
+      var viewport = {
+        bottom: 0,
+        left: 0,
+        right: 0,
+        top: 0,
+      };
 
       function dragElement(elmnt) {
-        console.log(elmnt);
         var pos1 = 0,
           pos2 = 0,
           pos3 = 0,
           pos4 = 0;
         if (document.getElementById(elmnt.id + "Header")) {
-          // if present, the header is where you move the DIV from:
+          /* if present, the header is where you move the DIV from:*/
           document.getElementById(elmnt.id + "Header").onmousedown =
             dragMouseDown;
         } else {
-          // otherwise, move the DIV from anywhere inside the DIV:
+          /* otherwise, move the DIV from anywhere inside the DIV:*/
           elmnt.onmousedown = dragMouseDown;
         }
 
         function dragMouseDown(e) {
           e = e || window.event;
-          e.preventDefault();
           // get the mouse cursor position at startup:
           pos3 = e.clientX;
           pos4 = e.clientY;
+
+          // store the current viewport and element dimensions when a drag starts
+          rect = elmnt.getBoundingClientRect();
+          viewport.bottom = window.innerHeight - PADDING;
+          viewport.left = PADDING;
+          viewport.right = window.innerWidth - PADDING;
+          viewport.top = PADDING;
+
           document.onmouseup = closeDragElement;
           // call a function whenever the cursor moves:
           document.onmousemove = elementDrag;
         }
 
         function elementDrag(e) {
+          document.querySelector('body').classList.add('noScroll');
           e = e || window.event;
-          e.preventDefault();
           // calculate the new cursor position:
           pos1 = pos3 - e.clientX;
           pos2 = pos4 - e.clientY;
           pos3 = e.clientX;
           pos4 = e.clientY;
-          // set the element's new position:
-          elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-          elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+
+          // check to make sure the element will be within our viewport boundary
+          var newLeft = elmnt.offsetLeft - pos1;
+          var newTop = elmnt.offsetTop - pos2;
+
+          if (
+            newLeft < viewport.left ||
+            newTop < viewport.top ||
+            newLeft + rect.width > viewport.right ||
+            newTop + rect.height > viewport.bottom
+          ) {
+            // the element will hit the boundary, do nothing...
+          } else {
+            // set the element's new position:
+            elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+            elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+          }
         }
 
         function closeDragElement() {
-          // stop moving when mouse button is released:
+          document.querySelector('body').classList.remove('noScroll');
+          /* stop moving when mouse button is released:*/
           document.onmouseup = null;
           document.onmousemove = null;
         }
@@ -218,7 +247,7 @@ export default {
 <style lang="scss" scoped>
 .modal {
   display: flex;
-  position: absolute;
+  position: fixed;
   bottom: 16px;
   right: 16px;
   z-index: 99999;
